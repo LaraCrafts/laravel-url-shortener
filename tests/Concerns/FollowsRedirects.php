b7cdb916fce15b6fe5007e5f51e908e368fc58e6
Choraimy Kroonstuiver <choraimy@23g.nl>
2019-04-27 12:37:47 +0200

    🔧 Moved assertRedirect to its own constraint

--- tests/Concerns/FollowsRedirects.php
+++ tests/Concerns/FollowsRedirects.php
@@ -2,38 +2,20 @@
 
 namespace LaraCrafts\UrlShortener\Tests\Concerns;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\TransferStats;
+use LaraCrafts\UrlShortener\Tests\Constraints\RedirectsTo;
 
 trait FollowsRedirects
 {
     /**
-     * Assert that given URL redirects to expected URL.
+     * Assert that the given value redirects to the expected value.
      *
      * @param \Psr\Http\Message\UriInterface|string $expected
      * @param \Psr\Http\Message\UriInterface|string $actual
      * @param int $redirects
      * @return void
      */
-    public static function assertRedirectsTo($expected, $actual, int $redirects)
+    public static function assertRedirectsTo($expected, $actual, int $redirects = 1)
     {
-        $client = new Client();
-        $expected = rtrim($expected, '/');
-        $stack = [];
-
-        $client->get($actual, [
-            'allow_redirects' => [
-                'max' => max($redirects, 5),
-            ],
-            'on_stats' => function (TransferStats $stats) use (&$stack) {
-                $stack[] = (string)$stats->getEffectiveUri();
-            },
-        ]);
-
-        if (($actualRedirects = count($stack) - 1) < $redirects) {
-            static::fail("Expected $redirects redirects, received $actualRedirects");
-        }
-
-        static::assertEquals($expected, rtrim($stack[$redirects], '/'));
+        static::assertThat($actual, new RedirectsTo($expected, $redirects));
     }
 }
