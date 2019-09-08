d0613d298736f2c4c21bd8b7bb5be5a0ed6f209e
Choraimy Kroonstuiver <choraimy@23g.nl>
2019-09-08 14:57:37 +0200

    ✨ Add Laravel 6 support (#35)

--- tests/Integration/IsGdShortenerTest.php
+++ tests/Integration/IsGdShortenerTest.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Promise\PromiseInterface;
 use LaraCrafts\UrlShortener\Http\IsGdShortener;
 use LaraCrafts\UrlShortener\Tests\Concerns\HasUrlAssertions;
-use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class IsGdShortenerTest extends TestCase
 {
