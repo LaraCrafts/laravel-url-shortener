c2b6b664ffbb4db28aebc1f27fcf30b5db4c743a
Choraimy Kroonstuiver <choraimy@live.nl>
2019-09-28 15:55:52 +0200

    🔧 Add request factory support

--- src/UrlShortenerServiceProvider.php
+++ src/UrlShortenerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace LaraCrafts\UrlShortener;
 
 use Illuminate\Support\ServiceProvider;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
 
 class UrlShortenerServiceProvider extends ServiceProvider
@@ -14,15 +15,15 @@ class UrlShortenerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->resolveUriFactory();
+        $this->resolveFactories();
     }
 
     /**
-     * Register a PSR-17 compatible URI factory.
+     * Register a PSR-17 compatible Request and URI factory.
      *
      * @return void
      */
-    protected function resolveUriFactory()
+    protected function resolveFactories()
     {
         if ($this->app->bound(UriFactoryInterface::class)) {
             return;
@@ -30,9 +31,11 @@ class UrlShortenerServiceProvider extends ServiceProvider
 
         if (class_exists('\GuzzleHttp\Psr7\HttpFactory')) {
             # Guzzle 7
+            $this->app->bind(RequestFactoryInterface::class, '\GuzzleHttp\Psr7\HttpFactory');
             $this->app->bind(UriFactoryInterface::class, '\GuzzleHttp\Psr7\HttpFactory');
         } elseif (class_exists('\Http\Factory\Guzzle\UriFactory')) {
             # HTTP Interop adapter for Guzzle 6
+            $this->app->bind(RequestFactoryInterface::class, '\Http\Factory\Guzzle\RequestFactory');
             $this->app->bind(UriFactoryInterface::class, '\Http\Factory\Guzzle\UriFactory');
         }
     }
